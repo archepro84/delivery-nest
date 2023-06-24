f77d7edd44abeeeda02eba875ffc4063b27f313e
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-06-24 15:54:36 +0900

    feat: Remove default customer files

--- apps/customer/src/customer.module.ts
+++ apps/customer/src/customer.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CustomerController } from './customer.controller';
-import { CustomerService } from './customer.service';
 
 @Module({
   imports: [],
-  controllers: [CustomerController],
-  providers: [CustomerService],
+  controllers: [],
+  providers: [],
 })
 export class CustomerModule {}
